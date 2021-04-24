65d2b6ba07235621680b50482b7eb2d4375ac605
vimarteleto <viniciusmarteleto@gmail.com>
2021-04-24 03:28:05 -0300

    Estrutura da pagina e estilizaçao

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+// date-fns para formatação de datas
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
+
+import styles from './styles.module.scss'
+
+export default function Header() {
+
+    // formatação de data com date-fns
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR
+    })
+
+
+    return (
+        <header className={styles.headerContainer}> 
+            <img src="/logo.svg" alt="Podcastr"/>
+
+            <p>O melhor para você ouvir sempre</p>
+            <span>{currentDate}</span>
+
+        </header>
+    )
+}
